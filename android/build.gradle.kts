a948d3fdf90e6691307d3d4d7ecc07b2c78c084d
Okikiola <okikday@gmail.com>
2026-08-23 00:01:15 +0100

    fix gradle error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,36 +11,36 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-        afterEvaluate {
-        val project = this // 'this' refers to the Project
-
-        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
-            val androidExtension =
-                project.extensions.getByName("android") as? com.android.build.gradle.LibraryExtension
-                    ?: project.extensions.getByName("android") as? com.android.build.gradle.AppExtension
-
-            androidExtension?.apply {
-                compileSdkVersion(36)
-                buildToolsVersion = "36.0.0"
-            }
-        }
-
-        if (project.extensions.findByName("android") != null) {
-            val androidExtension =
-                project.extensions.getByName("android") as? com.android.build.gradle.LibraryExtension
-                    ?: project.extensions.getByName("android") as? com.android.build.gradle.AppExtension
-
-            androidExtension?.apply {
-                if (namespace == null) {
-                    namespace = project.group.toString()
+    
+    if (project.name != "app") {
+        val configureAndroid = Action<Project> {
+            if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
+                val androidExt = extensions.findByName("android")
+                if (androidExt != null) {
+                    try {
+                        // Use reflection to bypass AGP 9.0 strict type removal while still fixing older plugins
+                        androidExt.javaClass.getMethod("setCompileSdkVersion", Int::class.java).invoke(androidExt, 36)
+                        androidExt.javaClass.getMethod("setBuildToolsVersion", String::class.java).invoke(androidExt, "36.0.0")
+                        
+                        val namespace = androidExt.javaClass.getMethod("getNamespace").invoke(androidExt)
+                        if (namespace == null) {
+                            androidExt.javaClass.getMethod("setNamespace", String::class.java).invoke(androidExt, group.toString())
+                        }
+                    } catch (e: Exception) {
+                        // Ignore if a modern plugin is already using the newer DSL structure
+                    }
                 }
             }
         }
+        
+        if (state.executed) {
+            configureAndroid.execute(this)
+        } else {
+            afterEvaluate(configureAndroid)
+        }
     }
-    // ============
-    buildDir = file("${rootProject.buildDir}/${project.name}")
-    evaluationDependsOn(":app")
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -48,4 +48,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
